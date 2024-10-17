20696632aa46a862420854c561636355799b9542
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-10-17 14:55:05 -0300

    carga inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PropiedadesListContainer from './PropiedadesListContainer/PropiedadesListContainer'
+
+function App() {
+
+
+  return (
+    <>
+      <PropiedadesListContainer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/PropiedadesListContainer/PropiedadesListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./PropiedadesListContainer.css"
+
+const PropiedadesListContainer = () => {
+  return (
+    <div>PropiedadesListContainer</div>
+  )
+}
+
+export default PropiedadesListContainer
